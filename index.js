64df6d1d534cb8b7f14698f3227c8a39a9446d16
s79651582 <s79651582@gmail.com>
2025-09-06 23:28:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from "express";
+import bodyParser from "body-parser";
 const dialogflow = require('@google-cloud/dialogflow');
 const uuid = require('uuid');
 require('dotenv').config();
